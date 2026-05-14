b0f7365e3ae3bcdf7ad45eeea88363129e82eb75
javierhuerta <javierhuerta91@gmail.com>
2026-05-14 10:18:13 -0400

    chore: remove legacy Django files and add OpenSpec commands

--- church_management/api.py
+++
@@ -1,5 +0,0 @@
-from ninja import NinjaAPI
-from calendar_app.api import router as calendar_router
-
-api = NinjaAPI(title="Church Management API", version="1.0")
-api.add_router("/events/", calendar_router)
